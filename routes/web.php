cbd12b7d4630f9a7f323d8e2740c60b02131ac6b
Thales Sousa <thales.b.sousa@gmail.com>
2024-12-16 09:20:34 -0300

    ultimo commit uhull

--- routes/web.php
+++ routes/web.php
@@ -18,42 +18,46 @@ Auth::routes();
 
 // Rotas públicas
 Route::get('/welcome', fn () => view('welcome'))->name('welcome');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Clientes
-Route::get('/clientes/criancas', [ClienteController::class, 'showCriancas'])->name('clientes.showCriancas');
-Route::get('/clientes/crianca/{cliente}', [ClienteController::class, 'crianca'])->name('clientes.crianca');
-Route::get('/clientes/{id}/criancas', [ClienteController::class, 'getCriancas'])->name('clientes.getCriancas');
-Route::get('/clientes/buscar', [ClienteController::class, 'buscar'])->name('clientes.buscar');
-Route::resource('/clientes', ClienteController::class);
+// Rotas protegidas
+Route::middleware(['auth'])->group(function () {
+    // Página inicial protegida
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Criancas
-Route::resource('/criancas', CriancaController::class);
-// Rota para editar a criança
-Route::get('/clientes/{cliente}/criancas/{crianca}/edit', [CriancaController::class, 'edit'])->name('criancas.edit');
+    // Clientes
+    Route::get('/clientes/criancas', [ClienteController::class, 'showCriancas'])->name('clientes.showCriancas');
+    Route::get('/clientes/crianca/{cliente}', [ClienteController::class, 'crianca'])->name('clientes.crianca');
+    Route::get('/clientes/{id}/criancas', [ClienteController::class, 'getCriancas'])->name('clientes.getCriancas');
+    Route::get('/clientes/buscar', [ClienteController::class, 'buscar'])->name('clientes.buscar');
+    Route::resource('/clientes', ClienteController::class);
 
-// Rota para atualizar a criança
-Route::put('/clientes/{cliente}/criancas/{crianca}', [CriancaController::class, 'update'])->name('criancas.update');
+    // Criancas
+    Route::resource('/criancas', CriancaController::class);
+    // Rota para editar a criança
+    Route::get('/clientes/{cliente}/criancas/{crianca}/edit', [CriancaController::class, 'edit'])->name('criancas.edit');
+    // Rota para atualizar a criança
+    Route::put('/clientes/{cliente}/criancas/{crianca}', [CriancaController::class, 'update'])->name('criancas.update');
 
-// Serviços
-Route::resource('/servicos', ServicoController::class);
+    // Serviços
+    Route::resource('/servicos', ServicoController::class);
 
-// Consumo
-Route::resource('/consumo', ConsumoController::class);
-Route::post('consumo/{consumo}/adicionar-tempo', [ConsumoController::class, 'adicionarTempo'])->name('consumo.adicionarTempo');
-Route::post('consumo/{consumo}/servico', [ConsumoController::class, 'servico'])->name('consumo.servico');
-Route::put('consumo/{id}/finalizar', [ConsumoController::class, 'finalizar'])->name('consumo.finalizar');
-Route::get('/consumo/{consumo}', [ConsumoController::class, 'show'])->name('consumo.show');
+    // Consumo
+    Route::resource('/consumo', ConsumoController::class);
+    Route::post('consumo/{consumo}/adicionar-tempo', [ConsumoController::class, 'adicionarTempo'])->name('consumo.adicionarTempo');
+    Route::post('consumo/{consumo}/servico', [ConsumoController::class, 'servico'])->name('consumo.servico');
+    Route::put('consumo/{id}/finalizar', [ConsumoController::class, 'finalizar'])->name('consumo.finalizar');
+    Route::get('/consumo/{consumo}', [ConsumoController::class, 'show'])->name('consumo.show');
 
-// Pagamentos
-Route::get('/pagamento/{consumo}', [ConsumoController::class, 'pagamento'])->name('consumo.pagamento');
-Route::post('/pagamento/{id}/confirmar', [ConsumoController::class, 'confirmarPagamento'])->name('pagamento.confirmar');
+    // Pagamentos
+    Route::get('/pagamento/{consumo}', [ConsumoController::class, 'pagamento'])->name('consumo.pagamento');
+    Route::post('/pagamento/{id}/confirmar', [ConsumoController::class, 'confirmarPagamento'])->name('pagamento.confirmar');
 
-// Relatórios
-Route::get('/relatorios', [ConsumoController::class, 'index'])->name('relatorios.index');
+    // Relatórios
+    Route::get('/relatorios', [ConsumoController::class, 'index'])->name('relatorios.index');
 
-// Recibos
-Route::get('/recibo/pdf/{id}', [ReciboController::class, 'gerarReciboPDF'])->name('recibo.pdf');
+    // Recibos
+    Route::get('/recibo/pdf/{id}', [ReciboController::class, 'gerarReciboPDF'])->name('recibo.pdf');
 
-// Usuários
-Route::resource('users', UserController::class);
+    // Usuários
+    Route::resource('users', UserController::class);
+});
